4c452e249e232a98cc3e5702375a6d171aa02f1c
Jason <jasonatoledo@gmail.com>
2023-02-12 01:04:00 -0800

    Update and rename dvd_rental_stored_procedure to dvd_rental_functions_and_stored_procedure.sql

--- dvd_rental_functions_and_stored_procedure.sql
+++ dvd_rental_functions_and_stored_procedure.sql
@@ -1,3 +1,110 @@
+-- create function to return next dvd in stock for a customer
+CREATE OR ALTER FUNCTION Fun_InStockDVDId 
+(@MemID numeric(12))
+
+-- return value has same length as DVDId max length
+RETURNS numeric(16)
+
+AS
+
+BEGIN
+
+	DECLARE @NextDVDId numeric(16);
+	-- ensure the memberId is part of the queue
+	IF EXISTS (SELECT MemberId FROM RentalQueue WHERE MemberId = @MemID)
+																		
+		-- set
+		SET @NextDVDId = -- select statement to return first DVDId in the queue
+						 -- that is available for the member id parameter
+							(SELECT	TOP 1 
+									RentalQueue.DVDId -- only select the first dvdid in the queue
+								FROM DVD
+									JOIN RentalQueue ON DVD.DVDId = RentalQueue.DVDId
+								WHERE RentalQueue.MemberId = @MemID
+									AND DVD.DVDQuantityOnHand >= 1 -- ensure DVD qty is at least 1
+								ORDER BY RentalQueue.QueuePosition
+							)
+	ELSE
+		SET @NEXTDVDId = NULL -- if member doesn't have DVDs in queue return NULL
+
+	RETURN @NextDVDId; -- function will return one of the two conditions
+
+
+
+-- create function to return # of additional DVDs that can be rented this month
+CREATE OR ALTER FUNCTION Fun_AdditionalDVDs
+(@MemID numeric(12))
+RETURNS numeric(2)  -- return value has max of 2 digits
+AS
+BEGIN
+
+	-- declare variables for different counts and limits
+	DECLARE @NumDVDsAvailable NUMERIC(2); -- this will be used as the return value
+	DECLARE @DVDLimitPerMonth NUMERIC(2); -- DVD limit per membership plan
+	DECLARE @DvdsRentedThisMonth NUMERIC(2); -- # of dvds member has rented this month
+	DECLARE @DVDsRemainingMonth NUMERIC(2); -- remaining dvds for this calendar month
+	DECLARE @DVDsOutRightNow NUMERIC(2); -- get number of dvds member has out currently
+	DECLARE @DVDsAtATimeValue NUMERIC(2); -- get current dvd limit based on member plan
+	-- need to check if the user has rental records out
+	IF @MemID IN (SELECT MemberId FROM Rental WHERE Rental.MemberId = @MemID)
+	BEGIN
+
+	-- get dvd limit for a member based on membership
+	SET @DVDLimitPerMonth = 
+		(SELECT  MembershipLimitPerMonth
+			FROM Membership
+			JOIN Member ON Membership.MembershipId = Member.MembershipId
+			WHERE MemberId = @Memid)
+
+	-- get the number of dvds rented this month based on members ship date
+	SET @DvdsRentedThisMonth =
+		(SELECT COUNT(DVDId) AS DVDRentedThisMonth
+			FROM Rental
+			WHERE RentalShippedDate IS NOT NULL
+			AND MemberId = @Memid
+			AND RentalShippedDate > EOMONTH(DATEADD(MONTH, -1, GETDATE()))
+			AND RentalShippedDate < EOMONTH(GETDATE()))
+	
+	-- get the number of remaining DVDs a member can rent this month
+	SET @DVDsRemainingMonth = @DVDLimitPerMonth - @DvdsRentedThisMonth
+
+	-- get # of dvds out by the member
+	SET @DVDsOutRightNow = (SELECT COUNT(DVDId) AS DVDsOut
+								FROM Rental
+								WHERE Rental.RentalReturnedDate IS NULL
+								AND Rental.MemberId = @MemID
+								GROUP BY Rental.MemberId)
+	-- get # of dvds a member can have at a time
+	SET @DVDsAtATimeValue = (SELECT Membership.DVDAtTime
+								FROM Membership
+								JOIN Member ON Membership.MembershipId = Member.MembershipId
+								WHERE Member.MemberId = @MemID)
+	-- calculate return value - if user does not have any rentals, then dvd at at time value
+	SET @NumDVDsAvailable = CASE WHEN @DVDsRemainingMonth < (@DVDsAtATimeValue - @DVDsOutRightNow)
+								 THEN @DVDsRemainingMonth
+								 WHEN (@DVDsAtATimeValue - @DVDsOutRightNow) < @DVDsRemainingMonth
+								 THEN (@DVDsAtATimeValue - @DVDsOutRightNow)
+								 END
+	 END
+	 -- if the member does not have any rentals then default to dvd at a time value per plan
+	 ELSE IF @MemID NOT IN	(SELECT MemberId FROM Rental WHERE Rental.MemberId = @MemID) 
+		 SET @NumDVDsAvailable =  (SELECT Membership.DVDAtTime
+									FROM Membership
+									JOIN Member ON Membership.MembershipId = Member.MembershipId
+									WHERE Member.MemberId = @MemID)
+     RETURN @NumDVDsAvailable;
+END;
+
+
+
+
+
+
+
+
+
+
+
 -- stored procedure to process lost DVD or return and determine number of DVDs to be sent
 CREATE OR ALTER PROCEDURE SP_ProcessDVDSendDVD
 	@P_MemberId NUMERIC     -- This parameter is the MemberId returning or lost a DVD
