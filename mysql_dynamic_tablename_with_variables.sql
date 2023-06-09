1259552da399d5271e9e324629947e6fb8d09a68
Jason <jasonatoledo@gmail.com>
2023-06-08 21:19:58 -0700

    Update mysql_dynamic_tablename_with_variables.sql
    add a 2nd query with an aggregate function

--- mysql_dynamic_tablename_with_variables.sql
+++ mysql_dynamic_tablename_with_variables.sql
@@ -8,3 +8,14 @@ SET @stmt1 :=  SUBSTRING_INDEX(CONCAT('select * from ',@table_name, ' LIMIT 100'
 
 PREPARE executable_stmt FROM @stmt1;
 EXECUTE executable_stmt;
+
+
+-- the second version aggregates the top 20 IPs with highest clicks in desc order
+SET @today_var := CONCAT(YEAR(CURRENT_DATE),0,MONTH(CURRENT_DATE),0,DAY(CURRENT_DATE));
+SET @table_name := CONCAT(CAST('clicks_' AS CHAR), CAST(@today_var AS SIGNED));
+SET @stmt1 :=  SUBSTRING_INDEX(CONCAT('select ip, cnt 
+					from (select nats_code, pid, ip, count(1) as cnt
+					from ',@table_name, 
+					' group by 1,2,3 order by 4 desc LIMIT 20)a '),"'",-1);
+PREPARE executable_stmt FROM @stmt1;
+EXECUTE executable_stmt;
